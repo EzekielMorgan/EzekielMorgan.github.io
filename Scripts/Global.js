6768156addefed2fe9d64ff4e68ab4addd0e535b
Ezekiel <emorgan.2026@chaffeer2.org>
2024-09-03 10:37:57 -0500

    Attempt to move version to a single script loaded on all pages

--- Scripts/Global.js
+++ Scripts/Global.js
@@ -1,7 +1,13 @@
 /*
+CONSTANT VARS
+*/
+
+const VERSION = "0.1.22"
+
+/*
 Theme
 */
-() => {
+(() => {
     const ThemeChangeButton = document.getElementById("ThemeButton")
 
     if (!ThemeChangeButton) {
@@ -18,4 +24,14 @@ Theme
             ThemeChangeButton.value = "Change Theme"
         }, 3000);
     }
-}
+})()
+
+(() => {
+    const footer = document.getElementById("footer")
+    if (!footer) {
+        return console.error("Page is missing the \"footer\"")
+    }
+    console.log("Footer found")
+
+    footer.innerText = VERSION
+})
