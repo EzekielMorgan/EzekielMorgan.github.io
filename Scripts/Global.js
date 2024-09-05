d7b3006ce442f20f6d4c2de1af65898700160009
Ezekiel <emorgan.2026@chaffeer2.org>
2024-09-05 10:07:42 -0500

    .

--- Scripts/Global.js
+++ Scripts/Global.js
@@ -2,7 +2,7 @@
 CONSTANT VARS
 */
 
-const VERSION = "0.1.42";
+const VERSION = "0.1.43";
 
 /*
 DEBOUNCE VARS
@@ -54,7 +54,7 @@ const UpdateFooter = async () => {
 Init
 */
 
-document.addEventListener("DOMContentLoaded", () => {
+Document.addEventListener("DOMContentLoaded", () => {
     BindThemeButton().then(() => {
         console.log("ThemeButton Bound")
     }).catch(console.error)
